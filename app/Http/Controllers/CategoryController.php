ef43c2116c407a809b4ba9b17e7e560d37fa0fb4
maparcepel <maparcepel@gmail.com>
2021-07-17 18:14:50 +0200

    categories ok

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,14 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Subcategory;
 
 class CategoryController extends Controller{
    
     public function get()
     {
-        return response('hola');
+        $categories = Category::all();
+
+        $categories = $categories->map(function ($category){
+            return $category->only(['id', 'name']);
+        });  
+
+        $categories_subcategories = $categories->map(function ($category){
+
+            $subcategories = Subcategory::where('category_id', '=', $category['id'])->get();
+
+            $subcategories = $subcategories->map(function ($subcategory){
+                return $subcategory->only(['id', 'name']);
+            });
+
+            return [
+                'category' => $category,
+                'subcategories' => $subcategories
+            ];
+        });
+
+        return response($categories_subcategories);
 
     }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class OrderItem extends Model
 {
     use HasApiTokens, HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'produsct_id',
+        'quantity',
+        'unit_price'
+    ];
 }
